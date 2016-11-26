2e4bcb66c171d33232e9f579b7a369b069a33345
Max Lee <mail@moep.tv>
2016-11-26 22:20:27 +0100

    Add cooldowns Also block players from directly teleporting after join, add some caching, update pom with dynamic version

--- src/main/java/net/md_5/janus/Main.java
+++ src/main/java/net/md_5/janus/Main.java
@@ -7,8 +7,14 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
-import org.bukkit.Bukkit;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -16,7 +22,6 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Sign;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -35,27 +40,39 @@ public class Main extends JavaPlugin implements Listener {
     private boolean portalTurnPlayer = false;
     private int portalDistance = 3;
     private boolean blockMessages = false;
+    private int cooldown = 10;
+    private String cooldownMessage = ChatColor.RED + "Please wait %time% more seconds before entering a portal!";
     private String noPermission = "You don't have permission to use Server Portals!";
     private String signIdentifier = "server";
 
-	HashSet<UUID> enteredPortal = new HashSet<UUID>();
-	
+	private Cache<UUID, Long> cooldownCache;
+    private LoadingCache<Location, Set<Block>> portalCache;
+
     @Override
     public void onEnable() {
-        Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+        cooldownCache = CacheBuilder.newBuilder().expireAfterWrite(cooldown, TimeUnit.SECONDS).build();
+        portalCache = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .build(new PortalCacheLoader());
+        getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
         getServer().getPluginManager().registerEvents(this, this);
         getConfig().addDefault("portalTurnPlayer", portalTurnPlayer);
         getConfig().addDefault("portalDistance", portalDistance);
+        getConfig().addDefault("cooldown", cooldown);
         getConfig().addDefault("blockMessages", blockMessages);
-        getConfig().addDefault("noPermission", noPermission);
+        getConfig().addDefault("lang.noPermission", noPermission);
+        getConfig().addDefault("lang.cooldownMessage", cooldownMessage);
         getConfig().addDefault("signIdentifier", signIdentifier);
         getConfig().options().copyDefaults(true);
         saveConfig();
-        portalTurnPlayer = getConfig().getBoolean("portalFlippPlayer");
+        portalTurnPlayer = getConfig().getBoolean("portalTurnPlayer");
         portalDistance = getConfig().getInt("portalDistance");
         if(portalDistance < 1) portalDistance = 1;
+        cooldown = getConfig().getInt("cooldown");
         blockMessages = getConfig().getBoolean("blockMessages");
-        noPermission = getConfig().getString("noPermission");
+        noPermission = ChatColor.translateAlternateColorCodes('&', getConfig().getString("lang.noPermission"));
+        cooldownMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("lang.cooldown"));
         signIdentifier = getConfig().getString("signIdentifier").toLowerCase();
     }
 
@@ -63,6 +80,10 @@ public class Main extends JavaPlugin implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (blockMessages) {
             event.setJoinMessage(null);
+        }
+        cooldownCache.put(event.getPlayer().getUniqueId(), System.currentTimeMillis());
+        if (event.getPlayer().getLocation().getBlock().getType() == Material.PORTAL) {
+            teleportOutOfPortal(event.getPlayer());
         }
     }
 
@@ -83,94 +104,115 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerPortalEnter(EntityPortalEnterEvent event) {
-    	if(event.getEntityType() == EntityType.PLAYER) {
-    		final UUID id = event.getEntity().getUniqueId();
-			if(!enteredPortal.contains(id)) {
-				enteredPortal.add(id);
-		        for (Block block : getPortalNear(event.getLocation())) {
-		            for (BlockFace bf : BlockFace.values()) {
-		                Block relative = block.getRelative(bf);
-		                if (relative.getType() == SIGN) {
-		                    Sign sign = (Sign) relative.getState();
-		                    if (sign.getLine(0).toLowerCase().equals("[" + signIdentifier + "]")) {
-		                        //
-		                    	Player player = (Player) event.getEntity();
-		                    	if(!player.hasPermission("janus.use")) {
-		                    		player.sendMessage(ChatColor.RED + noPermission);
-		                    		break;
-		                    	}
-		                    	Location location = player.getLocation();
-	        	            	float originalPitch = location.getPitch();
-	        	            	location.setPitch(0);
-	        	            	Vector vec = location.getDirection().multiply(portalDistance);
-	        	            	location = location.add(vec.multiply(-1));
-	        	            	location.setPitch(originalPitch);
-		                        if(portalTurnPlayer) {
-		                        	float yaw = location.getYaw();
-		                            if ((yaw += 180) > 360) {
-		                                yaw -= 360;
-		                            }
-		                            location.setYaw(yaw);
-		                        }
-		                        player.teleport(location, TeleportCause.PLUGIN);
-		                        ByteArrayOutputStream b = new ByteArrayOutputStream();
-		                        DataOutputStream out = new DataOutputStream(b);
-		                        try {
-		                            out.writeUTF("Connect");
-		                            out.writeUTF(sign.getLine(1));
-		                        } catch (IOException ex) {
-		                            // Impossible
-		                        }
-		
-		                        player.sendPluginMessage(this, "BungeeCord", b.toByteArray());
-		                        break;
-		                        //
-		                    }
-		                }
-		            }
-		        }		        
-		        Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-            		@Override
-            		public void run() {
-            			enteredPortal.remove(id);
-            		}
-		        }, 20L);
-	        }
-        }
-    }
-
-    private Set<Block> getPortalNear(Location loc) {
-    	World world = loc.getWorld();
-    	int x = loc.getBlockX();
-    	int y = loc.getBlockY();
-    	int z = loc.getBlockZ();
-        byte b0 = 0;
-        byte b1 = 0;
-        if (world.getBlockAt(x - 1, y, z).getType() == FRAME || world.getBlockAt(x + 1, y, z).getType() == FRAME) {
-            b0 = 1;
-        }
-        if (world.getBlockAt(x, y, z - 1).getType() == FRAME || world.getBlockAt(x, y, z + 1).getType() == FRAME) {
-            b1 = 1;
+    	if(!(event.getEntity() instanceof Player)) {
+            return;
         }
 
-        Set<Block> blocks = new HashSet<Block>();
-
-        if (world.getBlockAt(x - b0, y, z - b1).getType() == Material.AIR) {
-            x -= b0;
-            z -= b1;
+        Player player = (Player) event.getEntity();
+        if (cooldown > 0 && cooldownCache.getIfPresent(player.getUniqueId()) != null) {
+            int remaining = (int) (cooldown - (System.currentTimeMillis() - cooldownCache.getIfPresent(player.getUniqueId()) / 1000));
+            if (remaining >= 0) {
+                player.sendMessage(cooldownMessage.replace("%time%", String.valueOf(remaining + 1)));
+                return;
+            }
         }
 
-        for (byte i = -1; i <= 2; ++i) {
-            for (byte j = -1; j <= 3; ++j) {
-                boolean flag = i == -1 || i == 2 || j == -1 || j == 3;
+        for (Block block : getPortalNear(event.getLocation())) {
+            for (BlockFace bf : BlockFace.values()) {
+                Block relative = block.getRelative(bf);
+                if (relative.getType() == SIGN) {
+                    Sign sign = (Sign) relative.getState();
+                    if (sign.getLine(0).toLowerCase().equals("[" + signIdentifier + "]")) {
+                        if(!player.hasPermission("janus.use")) {
+                            player.sendMessage(ChatColor.RED + noPermission);
+                            break;
+                        }
 
-                if (i != -1 && i != 2 || j != -1 && j != 3) {
-                    if (flag) {
-                        blocks.add(world.getBlockAt(x + b0 * i, y + j, z + b1 * i));
+                        teleportOutOfPortal(player);
+                        ByteArrayOutputStream b = new ByteArrayOutputStream();
+                        DataOutputStream out = new DataOutputStream(b);
+                        try {
+                            out.writeUTF("Connect");
+                            out.writeUTF(sign.getLine(1));
+                        } catch (IOException ex) {
+                            // Impossible
+                        }
+
+                        cooldownCache.put(player.getUniqueId(), System.currentTimeMillis());
+                        player.sendPluginMessage(this, "BungeeCord", b.toByteArray());
+                        break;
+                        //
                     }
                 }
             }
         }
+    }
+
+    private Set<Block> getPortalNear(Location location) {
+        Set<Block> blocks = null;
+        try {
+            blocks = portalCache.get(location.getBlock().getLocation());
+        } catch (ExecutionException e) {
+            getLogger().log(Level.SEVERE, "Error while getting the portal near " + location, e);
+        }
+        if (blocks == null) {
+            return new HashSet<>();
+        }
         return blocks;
+    }
+
+    private void teleportOutOfPortal(Player player) {
+        Location location = player.getLocation();
+        float originalPitch = location.getPitch();
+        location.setPitch(0);
+        Vector vec = location.getDirection().multiply(portalDistance);
+        location = location.add(vec.multiply(-1));
+        location.setPitch(originalPitch);
+        if(portalTurnPlayer) {
+            float yaw = location.getYaw();
+            if ((yaw += 180) > 360) {
+                yaw -= 360;
+            }
+            location.setYaw(yaw);
+        }
+        player.teleport(location, TeleportCause.PLUGIN);
+    }
+
+    private class PortalCacheLoader extends CacheLoader<Location, Set<Block>> {
+        @Override
+        public Set<Block> load(Location loc) throws Exception {
+            World world = loc.getWorld();
+            int x = loc.getBlockX();
+            int y = loc.getBlockY();
+            int z = loc.getBlockZ();
+            byte b0 = 0;
+            byte b1 = 0;
+            if (world.getBlockAt(x - 1, y, z).getType() == FRAME || world.getBlockAt(x + 1, y, z).getType() == FRAME) {
+                b0 = 1;
+            }
+            if (world.getBlockAt(x, y, z - 1).getType() == FRAME || world.getBlockAt(x, y, z + 1).getType() == FRAME) {
+                b1 = 1;
+            }
+
+            Set<Block> blocks = new HashSet<>();
+
+            if (world.getBlockAt(x - b0, y, z - b1).getType() == Material.AIR) {
+                x -= b0;
+                z -= b1;
+            }
+
+            for (byte i = -1; i <= 2; ++i) {
+                for (byte j = -1; j <= 3; ++j) {
+                    boolean flag = i == -1 || i == 2 || j == -1 || j == 3;
+
+                    if (i != -1 && i != 2 || j != -1 && j != 3) {
+                        if (flag) {
+                            blocks.add(world.getBlockAt(x + b0 * i, y + j, z + b1 * i));
+                        }
+                    }
+                }
+            }
+            return blocks;
+        }
     }
 }
